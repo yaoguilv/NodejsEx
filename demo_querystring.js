ae4d338d40213f463b7929aae1358ce9a659e2f4
yaoguilv <yaoguilv@gmail.com>
2018-09-03 20:18:43 +0800

    add cross domain code to demo_querystring.js

--- demo_querystring.js
+++ demo_querystring.js
@@ -4,7 +4,12 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.setHeader("X-Powered-By",' 3.2.1');
+    res.setHeader("Content-Type", "text/html");
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.end(txt);
